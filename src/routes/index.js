d57ca664718fa8714af480bae5156beff75d7506
fatih celik <fhcelik@gmail.com>
2020-05-18 13:00:08 -0600

    add rating

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,15 +5,17 @@ const { version: apiVersion } = require('../../package');
 //const ErrorHandler = require('./middleware/errorHandler');
 //const login = require('./middleware/login');
 const beers = require('./beers');
-const errorHandler = require('./middleware/errorHandler')
+const rating = require('./rating');
+const authentication = require('./middleware/authentication');
+const errorHandler = require('./middleware/errorHandler');
 
 router.get('/', (req, res, next) => {
-
   res.send(`API v${apiVersion}`);
 });
 
+router.use('/login',authentication);
 router.use('/beers', beers);
-
+router.use('/rating', rating);
 router.use(errorHandler);
 
 module.exports = router;
---
+++ src/routes/rating.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const router = require('express').Router();
+const Datastore = require('nedb');
+const {validateSchema, ratingSchema} = require('./middleware/validateSchema');
+const { addRating, getRating } = require('../facade/rating')
+
+const db = new Datastore({
+    fileName: './rating.db',
+    autoLoad: true
+});
+
+router.put('/:id', validateSchema(ratingSchema), async (req, res, next) => {
+    const { id: beerId } = req.params;
+    const { rating: rating, comment: comment} = req.body
+    
+    db.insert({id:beerId, rating:rating, comment:comment}, 
+        function(err,doc){
+            res.sendStatus(204);
+    });
+    
+});
+
+router.get('/:id', async (req, res, next) => {
+    const { id: beerId } = req.params;
+
+    db.find({id:beerId}, 
+        function(err,doc){
+            res.json(doc)
+    });
+});
+
+module.exports = router;
