ca5f5afe3e6ef3c281d4fb906c21caec1a3bebab
fatih celik <fhcelik@gmail.com>
2020-05-18 11:50:43 -0600

    add beers

--- src/facade/beers.js
+++ src/facade/beers.js
@@ -1,11 +1,12 @@
 'use strict';
 const R = require('ramda');
 
-exports.propsOfBeers = beers =>{
+exports.propsOfBeers = (beers) =>{
     const arrayOfBeerProps = R.map(
             R.pick(['id', 'name', 'description', 'first_brewed', 'food_pairings']), 
             R.prop('data',beers)
         );
-    
+
     return arrayOfBeerProps;
+
 }
--- src/routes/beers.js
+++ src/routes/beers.js
@@ -3,24 +3,30 @@
 const router = require('express').Router();
 const axios = require('axios');
 const R = require('ramda');
+const cache = require('memory-cache');
 const { propsOfBeers } = require('../facade/beers')
 
-router.get('/', (req, res, next) => {
-  const {q: beerName} = req.query;
+  router.get('/', (req, res, next) => {
+    const {q: beerName} = req.query;
 
-  axios.get('https://api.punkapi.com/v2/beers?', 
-  { headers: {
-      'x-ratelimit-limit': 3600,
-      'x-ratelimit-remaining': 3587
-    },
-    params: {
-      'beer_name':beerName
+    if(cache.get(beerName)){
+      res.json(cache.get(beerName));
     }
-  })
-  .then(propsOfBeers)
-  .then(props => res.json(props))
-  .catch(next)
-});
-
+    axios.get('https://api.punkapi.com/v2/beers?', 
+    { headers: {
+        'x-ratelimit-limit': 3600,
+        'x-ratelimit-remaining': 3587
+      },
+      params: {
+        'beer_name':beerName
+      }
+    })
+    .then(propsOfBeers)
+    .then(props => {
+      cache.put(beerName,props,3600);
+      res.json(props);
+    })
+    .catch(next)
+  });
 
 module.exports = router;
