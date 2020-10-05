98c54f885ebf6d8aa0a54b9663fc8dfb06b8325b
fatih celik <fhcelik@gmail.com>
2020-10-04 23:08:50 -0600

    code cleanup

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,18 +2,14 @@
 
 const router = require('express').Router();
 const { version: apiVersion } = require('../../package');
-//const authentication = require('./middleware/authentication');
 const errorHandler = require('./middleware/errorHandler');
 const emission = require('./emission');
-//const rating = require('./rating');
 
 router.get('/', (req, res, next) => {
   res.send(`API v${apiVersion}`);
 });
 
-//router.use('/login',authentication);
 router.use('/emission', emission);
-//router.use('/rating', rating);
 router.use(errorHandler);
 
 module.exports = router;
